f8fa5d706f6500f63cde672e8b141f07fe0d6af7
Michael Haas <micha2718l@gmail.com>
2019-07-30 23:44:21 -0500

    fixing phi function, was missing a and x

--- unophysics/_quantum.py
+++ unophysics/_quantum.py
@@ -14,5 +14,7 @@ def E(phi_in, v_in, mass, var, low, high):
 def P(phi_in, psi_in, var, low, high):
     return (sp.Abs(sp.integrate(phi_in * psi_in, (var, low, high)))**2)/sp.integrate(psi_in * psi_in, (var, low, high))
 
-def phi(n):
+def phi(n, a=1, x=None):
+    if not x:
+        x = sp.symbols('x')
     return (sp.sqrt(2)/sp.sqrt(a))*sp.sin((n * sp.pi * x) / a)
