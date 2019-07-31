8b6e1325c79baf25c51e58b6035e2b5cf2443299
michael <micha2718l@gmail.com>
2019-07-30 22:42:50 -0500

    version.... ugh...

--- setup.py
+++ setup.py
@@ -46,10 +46,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/micha2718l/unophysics',
-<<<<<<< HEAD
     version='0.2.0',
-=======
-    version='0.1.5',
->>>>>>> master
     zip_safe=False,
 )
