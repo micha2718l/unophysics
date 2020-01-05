7f1d322a5113f12aeb2a65ee1666f8b5a1021b01
michael <micha2718l@gmail.com>
2020-01-04 21:23:21 -0600

    version bumping 💪

--- setup.py
+++ setup.py
@@ -47,6 +47,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/micha2718l/unophysics',
-    version='0.4.9',
+    version='0.5.3',
     zip_safe=False,
 )
