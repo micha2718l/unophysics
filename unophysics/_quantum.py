90e37b008aee5d86d73803bef5732c8c4eedebad
Michael Haas <micha2718l@gmail.com>
2020-02-22 22:21:02 -0600

    default n=1 for phi

--- unophysics/_quantum.py
+++ unophysics/_quantum.py
@@ -14,7 +14,7 @@ def E(phi_in, v_in, mass, var, low, high):
 def P(phi_in, psi_in, var, low, high):
     return (sp.Abs(sp.integrate(phi_in * psi_in, (var, low, high)))**2)/sp.integrate(psi_in * psi_in, (var, low, high))
 
-def phi(n, a=1, x=None):
+def phi(n=1, a=1, x=None):
     if int(n) != n:
         raise ValueError("n must be integer.")
 
