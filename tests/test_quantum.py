fedd6227aaa527be57cc6cdedc3d84f2122e85ed
Michael Haas <micha2718l@gmail.com>
2020-02-24 20:35:14 -0600

    refactor x out of class for other classes

--- tests/test_quantum.py
+++ tests/test_quantum.py
@@ -2,22 +2,23 @@ from unophysics import quantum
 import sympy as sp
 import pytest
 
+x = sp.symbols('x')
+
 class TestPhi:
-    x = sp.symbols('x')
 
     def test_phi_n1(self):
-        phi_1 = sp.sqrt(2) * sp.sin(sp.pi * self.x)
-        phi = quantum.phi(n=1, x=self.x)
+        phi_1 = sp.sqrt(2) * sp.sin(sp.pi * x)
+        phi = quantum.phi(n=1, x=x)
         assert phi == phi_1, "Should be sqrt(2)*sin(pi*x)"
 
     def test_phi_n2(self):
-        phi_2 = sp.sqrt(2) * sp.sin(2 * sp.pi * self.x)
-        phi = quantum.phi(n=2, x=self.x)
+        phi_2 = sp.sqrt(2) * sp.sin(2 * sp.pi * x)
+        phi = quantum.phi(n=2, x=x)
         assert phi == phi_2, "Should be sqrt(2)*sin(2*pi*x)"
 
     def test_phi_n1_a5(self):
-        phi_2 = sp.sqrt(10) * sp.sin(sp.pi * self.x /5) / 5
-        phi = quantum.phi(n=1, a=5, x=self.x)
+        phi_2 = sp.sqrt(10) * sp.sin(sp.pi * x /5) / 5
+        phi = quantum.phi(n=1, a=5, x=x)
         assert phi == phi_2, "Should be sqrt(2)*sin(2*pi*x)"
 
     def test_phi_non_integer(self):
@@ -27,6 +28,6 @@ class TestPhi:
         assert str(e.value) == "n must be integer."
 
     def test_phi_no_args(self):
-        phi = sp.sqrt(2) * sp.sin(sp.pi * self.x)
+        phi = sp.sqrt(2) * sp.sin(sp.pi * x)
         phi_no_args = quantum.phi()
         assert phi == phi_no_args, "Should be sqrt(2)*sin(pi*x)"
